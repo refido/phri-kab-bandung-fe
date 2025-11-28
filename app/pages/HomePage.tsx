0b29e5a927b74b4ca8ef6840537e8c3d8033e566
refido <revidoberliano@gmail.com>
2025-11-28 19:05:49 +0700

    feat: update HomePage to use local background image and reorganize imports

--- app/pages/HomePage.tsx
+++ app/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import {
-  ArrowRight, Building2,
+  ArrowRight,
+  Building2,
   FileCheck,
   Handshake,
   Info,
   Users,
-  Utensils
+  Utensils,
 } from "lucide-react";
 import { CallToAction } from "../../components/CallToAction";
 import { Hero } from "../../components/Hero";
@@ -70,7 +71,7 @@ export function HomePage({ onNavigate }: HomePageProps) {
       <Hero
         variant="slateWithImage"
         size="large"
-        backgroundImage="https://images.unsplash.com/photo-1618773928121-c32242e63f39?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        backgroundImage="/background.jpeg"
         title={translations.hero.title}
         description={translations.hero.subtitle}
         buttons={[
